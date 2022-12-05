2cd389df2b08d555ffef7d778043d6d308cda117
Ganesh Babu G <ganeshbabuNN@users.noreply.github.com>
2022-12-05 21:41:14 +0530

    Update R Pratices_ Mr Robot.R

--- R Training Mr Robot/R Pratices_ Mr Robot.R
+++ R Training Mr Robot/R Pratices_ Mr Robot.R
@@ -1,6 +1,41 @@
+#05dec2022
 #comments 
+#every line to be started # for the comments
+#3+3
 
-#ganesh 
-#hello world
-#hello world
-#how are yo
+#structure for R program
+
+##1st way - calculator way
+3+3
+print("hello Robot")
+
+hello =3 #store a value in a variable using = sign or <- (less with -)
+hello<-3
+print(hello)
+
+##2nd way - Function
+hello = function(){
+  print("Hello vaihav")
+}
+
+hello()
+#hello is function name
+#function() is keyword or object
+
+#3rd way - using objects (class and methods)
+employee =list(name="vaibhav",age=32,contact="93838",persontype="robot")
+class(employee) <-'empinfo'
+
+employee #calling the oops ways
+
+employee.status <- function(){
+  print("print the employee status")
+}
+
+employee
+
+employee.status() #method 
+
+#there two types of objects s3 and s4 objects types
+
+
